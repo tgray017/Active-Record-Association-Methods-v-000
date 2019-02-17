13ed4de6f2112b5bf0641e6c2e8b71d7f4db7191
tgray017 <tgray017@gmail.com>
2019-02-17 16:12:12 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,11 +3,11 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.songs.size
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.artists.size
   end
 
   def all_artist_names
